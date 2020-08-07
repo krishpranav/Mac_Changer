e6f7708dba6e5733ff7bcebca0fe11f0b0d12626
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-07 19:22:12 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,9 +1,15 @@
 import subprocess
-import optparse
+import time
 
 inteface = input("Enter Your Interface > ")
 new_mac = input("Enter Your New Mac > ")
 
+print("-" * 50)
+time.sleep(5)
+print("Chainging Your Mac Address...")
+time.sleep(5)
+print("-" * 50)
+time.sleep(5)
 subprocess.call("ifconfig" + inteface + "down")
 subprocess.call("ifconfig" + inteface + "hw" + "ether" + new_mac)
 subprocess.call("ifconfig" + inteface + "up")
