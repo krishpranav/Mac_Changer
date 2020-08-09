2fc7c11be65208ee407582432acd83a93fe1a912
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-09 15:12:26 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,10 @@ import random
 
 os.system("figle MAC CHANGER")
 
+def author():
+  print("My Github Link https://www.github.com/krishpranav")
+  print("Do not foget To follow me :)")
+
 inteface = input("Enter Your Interface > ")
 new_mac = input("Enter Your New Mac > ")
 
@@ -17,5 +21,4 @@ subprocess.call("ifconfig" + inteface + "down")
 subprocess.call("ifconfig" + inteface + "hw" + "ether" + new_mac)
 subprocess.call("ifconfig" + inteface + "up")
 print("Successfully Chaged Your Mac To > " + new_mac)
-print("My Github Link https://www.github.com/krishpranav")
-print("Do not foget To follow me :)")
+author()
